3e3c48b1ebff8f1d5e3ccb86d25c5cad5910cd7e
Janith-D <janetdgo2001@gmail.com>
2026-03-05 22:35:55 +0530

    resume

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -202,8 +202,8 @@ const Home = React.forwardRef((props, ref) => {
                 View My Work
               </a>
               <a
-                href="https://google.com"
-                download
+                href="/JanithDharmasiri.pdf"
+                download="JanithDharmasiri.pdf"
                 className="px-6 py-3 rounded-full text-lg font-medium text-black bg-white 
                 hover:bg-gray-200 shadow-lg hover:scale-105 transition-all"
               >
