fc5c6d34e4346ee568b92dc4b572a008776de26a
Janith-D <janetdgo2001@gmail.com>
2026-03-07 11:41:41 +0530

    Updated CV

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -202,8 +202,8 @@ const Home = React.forwardRef((props, ref) => {
                 View My Work
               </a>
               <a
-                href="/JanithDharmasiri.pdf"
-                download="JanithDharmasiri.pdf"
+                href="/JanithDharmasiri (1).pdf"
+                download="JanithDharmasiri (1).pdf"
                 className="px-6 py-3 rounded-full text-lg font-medium text-black bg-white 
                 hover:bg-gray-200 shadow-lg hover:scale-105 transition-all"
               >
